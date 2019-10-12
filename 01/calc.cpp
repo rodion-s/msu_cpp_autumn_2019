de1b97b73861463501d4673d6a8b328f3652e133
Rodion <rodion13par@gmail.com>
2019-10-12 18:21:35 +0300

    calc

--- 01/calc.cpp
+++ 01/calc.cpp
@@ -1,18 +1,17 @@
 /*
  S     --> T{+T | -T}
  T     --> F{*F | /F}
- F     --> -F | digit
- digit --> 0|1|....|INT_MAX
+ F     --> -F | NUMBER
+ NUMBER --> 0|1|....|INT_MAX
 */
 #include <iostream>
 #include <stack>
-#include <stdexcept>
 using namespace std;
 
 void gc(char *input, int &idx) {
-    while (isspace(input[idx++]));
+    while (isspace((unsigned char)input[idx++]));
 }
-void DIGIT(char *input, int &idx, stack<int> &my_stack) {
+void NUMBER(char *input, int &idx, stack<int> &my_stack) {
     static int current_num = 0;
     bool modified = false;
     while (isdigit((unsigned char)input[idx])) {
@@ -20,16 +19,16 @@ void DIGIT(char *input, int &idx, stack<int> &my_stack) {
         current_num = current_num * 10 + (input[idx++] - '0');
     }
     if (!modified) {
-        throw invalid_argument("invalid_argument");
+        throw "invalid_argument";
     }
     my_stack.push(current_num);
     current_num = 0;
-    while (isspace(input[idx])) {
+    while (isspace((unsigned char)input[idx])) {
         ++idx;
     }
 }
 void F(char *input, int &idx, stack<int> &my_stack) {
-    while (isspace(input[idx])) {
+    while (isspace((unsigned char)input[idx])) {
         ++idx;
     }
     if (input[idx] == '-') {
@@ -38,7 +37,7 @@ void F(char *input, int &idx, stack<int> &my_stack) {
         int op = my_stack.top(); my_stack.pop();
         my_stack.push(-op);
     } else {
-        DIGIT(input, idx, my_stack);
+        NUMBER(input, idx, my_stack);
     }
 }
 void T(char *input, int &idx, stack<int> &my_stack) {
@@ -72,19 +71,19 @@ void S(char *input, int &idx, stack<int> &my_stack) {
         }
     }
     if (input[idx]) {
-        throw invalid_argument("invalid_argument");
+        throw "invalid_argument";
     }
 }
 int main(int argc, char **argv) {
     int idx = 0;
     stack<int> my_stack;
-    while (isspace(argv[1][idx])) {
+    while (isspace((unsigned char)argv[1][idx])) {
         ++idx;
     }
     try {
         S(argv[1], idx, my_stack);
-    } catch (const invalid_argument &exc) {
-        cout << exc.what();
+    } catch (const char *exc) {
+        cout << exc;
         return 1;
     }
     cout << my_stack.top();
