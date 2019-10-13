1232b65eaaafc1e33ed70945624fda2825cade67
Rodion <rodion13par@gmail.com>
2019-10-14 00:44:17 +0300

    fixed

--- 01/calc.cpp
+++ 01/calc.cpp
@@ -1,19 +1,21 @@
 /*
- S     --> T{+T | -T}
- T     --> F{*F | /F}
- F     --> -F | NUMBER
+ EXPR     --> TERM{+TERM | -TERM}
+ TERM     --> FACTOR{*FACTOR | /FACTOR}
+ FACTOR     --> -FACTOR | NUMBER
  NUMBER --> 0|1|....|INT_MAX
 */
 #include <iostream>
 #include <stack>
+#include <stdexcept>
+#include <string>
 using namespace std;
 
-void gc(char *input, int &idx) {
+void GET_LEX(const char *input, int &idx) {
     while (isspace((unsigned char)input[idx++]));
 }
-void NUMBER(char *input, int &idx, stack<int> &my_stack) {
+void NUMBER(const char *input, int &idx, stack<int> &my_stack) {
     if (!isdigit((unsigned char)input[idx])) {
-        throw "invalid_argument";
+        throw invalid_argument(string("error on lexeme: ") + input[idx]);
     }
     int current_num = 0;
     while (isdigit((unsigned char)input[idx])) {
@@ -24,25 +26,25 @@ void NUMBER(char *input, int &idx, stack<int> &my_stack) {
         ++idx;
     }
 }
-void F(char *input, int &idx, stack<int> &my_stack) {
+void FACTOR(const char *input, int &idx, stack<int> &my_stack) {
     while (isspace((unsigned char)input[idx])) {
         ++idx;
     }
     if (input[idx] == '-') {
         ++idx;
-        F(input, idx, my_stack);
+        FACTOR(input, idx, my_stack);
         int op = my_stack.top(); my_stack.pop();
         my_stack.push(-op);
     } else {
         NUMBER(input, idx, my_stack);
     }
 }
-void T(char *input, int &idx, stack<int> &my_stack) {
-    F(input, idx, my_stack);
+void TERM(const char *input, int &idx, stack<int> &my_stack) {
+    FACTOR(input, idx, my_stack);
     while (input[idx] == '*' || input[idx] == '/') {
         char temp = input[idx];
-        gc(input, idx);
-        F(input, idx, my_stack);
+        GET_LEX(input, idx);
+        FACTOR(input, idx, my_stack);
         int op2 = my_stack.top(); my_stack.pop();
         int op1 = my_stack.top(); my_stack.pop();
         if (temp == '*') {
@@ -52,12 +54,13 @@ void T(char *input, int &idx, stack<int> &my_stack) {
         }
     }
 }
-void S(char *input, int &idx, stack<int> &my_stack) {
-    T(input, idx, my_stack);
+
+void EXPR(const char *input, int &idx, stack<int> &my_stack) {
+    TERM(input, idx, my_stack);
     while (input[idx] == '+' || input[idx] == '-') {
         char temp = input[idx];
-        gc(input, idx);
-        T(input, idx, my_stack);
+        GET_LEX(input, idx);
+        TERM(input, idx, my_stack);
         int op2 = my_stack.top(); my_stack.pop();
         int op1 = my_stack.top(); my_stack.pop();
         if (temp == '-') {
@@ -67,7 +70,7 @@ void S(char *input, int &idx, stack<int> &my_stack) {
         }
     }
     if (input[idx]) {
-        throw "invalid_argument";
+        throw invalid_argument(string("error on lexeme: ") + input[idx]);
     }
 }
 int main(int argc, char **argv) {
@@ -77,9 +80,9 @@ int main(int argc, char **argv) {
     }
     stack<int> my_stack;
     try {
-        S(argv[1], idx, my_stack);
-    } catch (const char *exc) {
-        cout << exc << endl;
+        EXPR(argv[1], idx, my_stack);
+    } catch (invalid_argument &exc) {
+        cout << exc.what() << endl;
         return 1;
     }
     cout << my_stack.top() << endl;
